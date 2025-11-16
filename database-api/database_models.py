e548afa21c5aaf94d2b9b3cb1c8839e9248ef060
LooseJuice1075 <hobo1075@gmail.com>
2025-11-15 21:33:19 -0500

    Database API initial work

---
+++ database-api/database_models.py
@@ -0,0 +1,75 @@
+import sqlalchemy
+from sqlalchemy import Column, ForeignKey, String, Integer, Boolean, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from pydantic import BaseModel
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "Users"
+
+    UserId = Column(Integer, primary_key=True, autoincrement=True)
+    DiscordId = Column(Integer)
+
+    def json(self):
+        json = dict()
+        json["user_id"] = self.UserId
+        json["discord_id"] = self.DiscordId
+
+
+class Team(Base):
+    __tablename__ = "Team"
+
+    TeamId = Column(Integer, primary_key=True, autoincrement=True)
+    TeamName = Column(String)
+    UserId = Column(Integer, ForeignKey(User.UserId))
+
+    PokemonId1 = Column(Integer, ForeignKey("Pokemon.PokemonId"))
+    PokemonId2 = Column(Integer, ForeignKey("Pokemon.PokemonId"))
+    PokemonId3 = Column(Integer, ForeignKey("Pokemon.PokemonId"))
+    PokemonId4 = Column(Integer, ForeignKey("Pokemon.PokemonId"))
+    PokemonId5 = Column(Integer, ForeignKey("Pokemon.PokemonId"))
+    PokemonId6 = Column(Integer, ForeignKey("Pokemon.PokemonId"))
+
+    def json(self):
+        json = dict()
+        json["team_id"] = self.TeamId
+        json["team_name"] = self.TeamName
+        json["user_id"] = self.UserId
+        json["pokemon_id1"] = self.PokemonId1
+        json["pokemon_id2"] = self.PokemonId2
+        json["pokemon_id3"] = self.PokemonId3
+        json["pokemon_id4"] = self.PokemonId4
+        json["pokemon_id5"] = self.PokemonId5
+        json["pokemon_id6"] = self.PokemonId6
+
+
+class Pokemon(Base):
+    __tablename__ = "Pokemon"
+
+    PokemonId = Column(Integer, primary_key=True, autoincrement=True)
+
+    Species = Column(String)
+
+    Ability = Column(String)
+
+    Move1 = Column(String)
+    Move2 = Column(String)
+    Move3 = Column(String)
+    Move4 = Column(String)
+
+    TeamId = Column(Integer, ForeignKey(Team.TeamId))
+    UserId = Column(Integer, ForeignKey(User.UserId))
+
+    def json(self):
+        json = dict()
+        json["pokemon_id"] = self.PokemonId
+        json["species"] = self.Species
+        json["ability"] = self.Ability
+        json["move1"] = self.Move1
+        json["move2"] = self.Move1
+        json["move3"] = self.Move3
+        json["move4"] = self.Move4
+        json["team_id"] = self.TeamId
+        json["user_id"] = self.UserId
+        return json
---
+++ database-api/database_operations.py
@@ -0,0 +1,239 @@
+import sqlalchemy
+from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+from database_models import *
+
+# User
+
+def get_users():
+    try:
+        global session
+        return session.query(User).all()
+    except Exception as e:
+        print(f"get_users failed: {e}")
+
+
+def get_user(user_id: int):
+    try:
+        global session
+        return session.query(User).where(User.UserId == user_id).first()
+    except Exception as e:
+        print(f"get_user failed: {e}")
+
+
+def get_user_by_discord_id(discord_id: int):
+    try:
+        global session
+        return session.query(User).where(User.DiscordId == discord_id).first()
+    except Exception as e:
+        print(f"get_user_by_discord_id failed: {e}")
+
+
+def user_update(user_id: int, updated_user: dict):
+    try:
+        global session
+        user = session.query(User).where(User.UserId == user_id).first()
+        user.UserId = int(updated_user["user_id"])
+        user.DiscordId = int(updated_user["discord_id"])
+        session.commit()
+        return user
+    except Exception as e:
+        print(f"user_update failed: {e}")
+
+
+def user_create(new_user: dict):
+    try:
+        global session
+        user = User(int(new_user["user_id"]), int(new_user["discord_id"]))
+        session.add(user)
+        session.commit()
+        return user
+    except Exception as e:
+        print(f"user_create failed: {e}")
+
+
+def user_delete(user_id: int):
+    try:
+        global session
+        user = session.query(User).where(User.UserId == user_id).first()
+        session.delete(user)
+        session.commit()
+    except Exception as e:
+        print(f"user_delete failed: {e}")
+
+
+# Team
+
+def get_teams():
+    try:
+        global session
+        return session.query(Team).all()
+    except Exception as e:
+        print(f"get_teams failed: {e}")
+
+
+def get_user_teams(user_id: int):
+    try:
+        global session
+        return session.query(Team).where(Team.UserId == user_id)
+    except Exception as e:
+        print(f"get_user_teams failed: {e}")
+
+
+def get_user_team(user_id: int, team_id: int):
+    try:
+        global session
+        return session.query(Team).where(Team.UserId == user_id).where(Team.TeamId == team_id)
+    except Exception as e:
+        print(f"get_user_team failed: {e}")
+
+
+def team_update(team_id: int, updated_team: dict):
+    try:
+        global session
+        team = session.query(Team).where(Team.TeamId == team_id).first()
+
+        team.teamId = int(updated_team["team_id"])
+        team.teamName = updated_team["team_name"]
+        team.userId = int(updated_team["user_id"])
+
+        team.PokemonId1 = updated_team["pokemon_id1"]
+        team.PokemonId2 = updated_team["pokemon_id2"]
+        team.PokemonId3 = updated_team["pokemon_id3"]
+        team.PokemonId4 = updated_team["pokemon_id4"]
+        team.PokemonId5 = updated_team["pokemon_id5"]
+        team.PokemonId6 = updated_team["pokemon_id6"]
+
+        session.commit()
+        return team
+    except Exception as e:
+        print(f"team_update failed: {e}")
+
+
+def team_create(new_team: dict):
+    try:
+        global session
+        team = Team()
+        team.teamId = int(new_team["team_id"])
+        team.teamName = new_team["team_name"]
+        team.userId = int(new_team["user_id"])
+
+        team.PokemonId1 = new_team["pokemon_id1"]
+        team.PokemonId2 = new_team["pokemon_id2"]
+        team.PokemonId3 = new_team["pokemon_id3"]
+        team.PokemonId4 = new_team["pokemon_id4"]
+        team.PokemonId5 = new_team["pokemon_id5"]
+        team.PokemonId6 = new_team["pokemon_id6"]
+
+        session.add(team)
+        session.commit()
+        return 
+    except Exception as e:
+        print(f"team_create failed: {e}")
+
+
+def team_delete(team_id: int):
+    try:
+        global session
+        team = session.query(Team).where(Team.TeamIdId == team_id).first()
+        session.delete(team)
+        session.commit()
+    except Exception as e:
+        print(f"team_delete failed: {e}")
+
+
+# Pokemon
+
+def get_all_pokemon():
+    try:
+        global session
+        return session.query(Pokemon).all()
+    except Exception as e:
+        print(f"get_all_pokemon failed: {e}")
+
+
+def get_pokemon(pokemon_id: int):
+    try:
+        global session
+        return session.query(Pokemon).where(Pokemon.PokemonId == pokemon_id).first()
+    except Exception as e:
+        print(f"get_pokemon failed: {e}")
+
+
+def get_pokemon_by_team(user_id: int, team_id: int):
+    try:
+        global session
+        return session.query(Pokemon).where(Pokemon.UserId == user_id).where(Pokemon.TeamId == team_id)
+    except Exception as e:
+        print(f"get_pokemon_by_team failed: {e}")
+
+
+def pokemon_update(pokemon_id: int, updated_pokemon: dict):
+    try:
+        global session
+        pokemon = session.query(Pokemon).where(Pokemon.PokemonId == pokemon_id).first()
+
+        pokemon.PokemonId = int(updated_pokemon["pokemon_id"])
+        pokemon.Species = updated_pokemon["species"]
+
+        pokemon.Ability = updated_pokemon["ability"]
+
+        pokemon.Move1 = updated_pokemon["move1"]
+        pokemon.Move2 = updated_pokemon["move2"]
+        pokemon.Move3 = updated_pokemon["move3"]
+        pokemon.Move4 = updated_pokemon["move4"]
+
+        pokemon.TeamId = int(updated_pokemon["team_id"])
+        pokemon.UserId = int(updated_pokemon["user_id"])
+
+        session.commit()
+        return pokemon
+    except Exception as e:
+        print(f"pokemon_update failed: {e}")
+
+
+def pokemon_create(new_pokemon: dict):
+    try:
+        global session
+        pokemon = Pokemon()
+
+        pokemon.PokemonId = int(new_pokemon["pokemon_id"])
+        pokemon.Species = new_pokemon["species"]
+
+        pokemon.Ability = new_pokemon["ability"]
+
+        pokemon.Move1 = new_pokemon["move1"]
+        pokemon.Move2 = new_pokemon["move2"]
+        pokemon.Move3 = new_pokemon["move3"]
+        pokemon.Move4 = new_pokemon["move4"]
+
+        pokemon.TeamId = int(new_pokemon["team_id"])
+        pokemon.UserId = int(new_pokemon["user_id"])
+        
+        session.add(pokemon)
+        session.commit()
+        return pokemon
+    except Exception as e:
+        print(f"pokemon_create failed: {e}")
+
+
+def pokemon_delete(pokemon_id: int):
+    try:
+        global session
+        pokemon = session.query(Pokemon).where(Pokemon.PokemonId == pokemon_id).first()
+        session.delete(pokemon)
+        session.commit()
+    except Exception as e:
+        print(f"pokemon_delete failed: {e}")
+
+
+def database_init():
+    DATABASE_URL = "sqlite:///../database/pokemon-bot.db"
+
+    engine = create_engine(DATABASE_URL, pool_size=5, pool_recycle=1800, pool_pre_ping=True)
+
+    session_factory = sessionmaker(autoflush=False, bind=engine)
+
+    global session
+    session = session_factory()
